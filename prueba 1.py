eb855708359116f781649853869a5dee0d33def3
Mauri-uno <rinaldi.el@hotmail.com>
2025-03-30 20:19:40 -0300

    se creo prueba1

---
+++ prueba 1.py
@@ -0,0 +1,7 @@
+print ("Hola amigo")
+print (" Que onda crack! ? ")
+p1= input ("¿Todo bien ?: ")
+if p1 =="si":
+    print ("Excelente Bro!")
+else: 
+    print ("Que mal, espero que se solucione pronto")
