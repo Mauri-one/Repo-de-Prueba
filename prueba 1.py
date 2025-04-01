10004bf873d7e904b37b2f34430b36e801b25ff3
Mauri-uno <rinaldi.el@hotmail.com>
2025-04-01 20:02:00 -0300

    Agregando mi-archivo.txt

--- prueba 1.py
+++
@@ -1,7 +0,0 @@
-print ("Hola amigo")
-print (" Que onda crack! ? ")
-p1= input ("¿Todo bien ?: ")
-if p1 =="si":
-    print ("Excelente Bro!")
-else: 
-    print ("Que mal, espero que se solucione prontooooooooooo")
--- prueba.py
+++
@@ -1,7 +0,0 @@
-print ("Hola Papu")
-print (" Que onda crack! ? ")
-p1= input ("¿Todo bien ?: ")
-if p1 =="si":
-    print ("Excelente Bro!")
-else: 
-    print ("Que mal, espero que se solucione pronto")
